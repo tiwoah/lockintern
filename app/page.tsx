c56f29d8f2881c1e4460bd045c9898ea1e0d0ef9
tiwoah <89097582+tiwoah@users.noreply.github.com>
2026-02-14 03:54:04 -0500

    chore: cleanup unused text prompt and button

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,11 @@
 import AudioToGemini from "@/components/AudioToGemini";
-import Example from "@/components/example";
 import MicRecorder from "@/components/mic-recorder";
 import TextToSpeech from "@/components/TextToSpeech";
-import { Button } from "@/components/ui/button";
-import { Mic } from "lucide-react";
 
 export default function Home() {
   return (
     <div>
       <h1>LockIntern</h1>
-      <Button>
-        <Mic />
-        Click me
-      </Button>
-      <Example />
       <TextToSpeech />
       <MicRecorder />
       <AudioToGemini />
